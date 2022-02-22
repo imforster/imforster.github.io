4928f1d3a276862f4699ffb4935081e0b6d3e69e
Ian Forster <ian.forster@gmail.com>
2022-02-21 18:55:17 -0800

    rebuilding site Mon 21 Feb 2022 18:55:17 PST

--- js/custom.js
+++ js/custom.js
@@ -1,24 +1,23 @@
-<script>
-    function handleClick() {
-      var form = document.querySelector("#formElem");
-      // var form = document.forms[0];
+function handleClick() {
+  var form = document.querySelector("#formElem");
+  // var form = document.forms[0];
 
-      data = {
-        fullname : form.querySelector('input[name="name"]').value,
-        email : form.querySelector('input[name="email"]').value,
-        organization: form.querySelector('input[name="organization"]').value
+  data = {
+    fullname: form.querySelector('input[name="name"]').value,
+    email: form.querySelector('input[name="email"]').value,
+    organization: form.querySelector('input[name="organization"]').value
         subject: form.querySelector('input[name="subject"]').value
         message: form.querySelector('input[name="message"]').value
-      }
+  }
 
-      let response = await fetch('http://localhost:8482/sendemail', {
-        method: 'POST', // or 'PUT'
-        headers: {
-          'Content-Type': 'application/json',
-                      },
-        body: JSON.stringify(data),
-      })
+  let response = await fetch('http://localhost:8482/sendemail', {
+    method: 'POST', // or 'PUT'
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  })
 
-      let ok = await response.ok; // read response body is ok
-    }
-</script>
+  let ok = await response.ok; // read response body is ok
+}
+
