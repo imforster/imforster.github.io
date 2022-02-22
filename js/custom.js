14c91681746394a4c39a854a0fde4809c3906794
Ian Forster <ian.forster@gmail.com>
2022-02-21 19:31:16 -0800

    rebuilding site Mon 21 Feb 2022 19:31:16 PST

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,6 @@
 function handleClick() {
   var form = document.querySelector("#formElem");
   // var form = document.forms[0];
-  const settings = {
-    method: 'POST',
-    headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-    }
-  };
-
   data = {
     fullname: form.querySelector('input[name="name"]').value,
     email: form.querySelector('input[name="email"]').value,
@@ -16,6 +8,17 @@ function handleClick() {
     subject: form.querySelector('input[name="subject"]').value,
     message: document.getElementById("message").value
   };
+
+  const settings = {
+    method: 'POST',
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    }
+    body: JSON.stringify(data)
+  };
+
+
 
   let response = fetch('http://localhost:8482/sendemail', settings);
 
