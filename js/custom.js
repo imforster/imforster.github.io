9b337a54f0f53b82ebf5d5896257480cc773c4c0
Ian Forster <ian.forster@gmail.com>
2022-02-21 18:23:29 -0800

    rebuilding site Mon 21 Feb 2022 18:23:29 PST

---
+++ js/custom.js
@@ -0,0 +1,25 @@
+<script>
+    formElem.onsubmit = async (e) => {
+      e.preventDefault();
+      var form = document.querySelector("#formElem");
+      // var form = document.forms[0];
+
+      data = {
+        fullname : form.querySelector('input[name="name"]').value,
+        email : form.querySelector('input[name="email"]').value,
+        organization: form.querySelector('input[name="organization"]').value
+        subject: form.querySelector('input[name="subject"]').value
+        message: form.querySelector('input[name="message"]').value
+      }
+
+      let response = await fetch('http://localhost:8482/sendemail', {
+        method: 'POST', // or 'PUT'
+        headers: {
+          'Content-Type': 'application/json',
+                      },
+        body: JSON.stringify(data),
+      })
+
+      let ok = await response.ok; // read response body is ok
+    };
+</script>
